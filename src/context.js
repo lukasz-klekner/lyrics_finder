5b78ea4fd36b382916ce0dc6614efc6100e45a65
Łukasz Klekner <lukaszklekner@gmail.com>
2021-09-12 13:26:18 +0200

    installs axios and gets axios query

--- src/context.js
+++ src/context.js
@@ -1,16 +1,19 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
+import axios from 'axios'
 
 export const Context = createContext()
 
 export const ContextProvider = ({ children }) => {
-  const [trackList, setTrackList] = useState([
-    {
-      track: { track_name: 'abc' },
-    },
-    {
-      track: { track_name: '123' },
-    },
-  ])
+  const [trackList, setTrackList] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then((response) => setTrackList(response.data.message.body.track_list))
+      .catch((error) => console.log(error))
+  }, [])
 
   return <Context.Provider value={{ trackList }}>{children}</Context.Provider>
 }
