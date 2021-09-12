08fedef3fc7fed107593908cf4f7a733154e5ced
Łukasz Klekner <lukaszklekner@gmail.com>
2021-09-12 16:57:01 +0200

    implements Lyrics components and uses it with routing

---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,80 @@
+import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Spinner from '../layout/Spinner'
+
+const Lyrics = () => {
+  const { id } = useParams()
+  const [lyrics, setLyrics] = useState({})
+  const [track, setTrack] = useState({})
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then((response) => setLyrics(response.data.message.body.lyrics))
+      .catch((error) => console.log(error))
+  }, [id])
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${id}&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then((response) => {
+        console.log(response.data.message.body.track)
+        setTrack(response.data.message.body.track)
+      })
+      .catch((error) => console.log(error))
+  }, [id])
+
+  if (
+    track === undefined ||
+    lyrics === undefined ||
+    Object.keys(track).length === 0 ||
+    Object.keys(lyrics).length === 0
+  ) {
+    return <Spinner />
+  } else {
+    return (
+      <>
+        <Link to='/' className='btn btn-dark btn-sm mb-4'>
+          Go Back
+        </Link>
+        <div className='card'>
+          <h5 className='card-header'>
+            {track.track_name} by
+            <span className='text-secondary'>{track.artist_name}</span>
+          </h5>
+          <div className='card-body'>
+            <p className='card-text'>{lyrics.lyrics_body}</p>
+          </div>
+        </div>
+
+        <ul className='list-group mt-3'>
+          <li className='list-group-item'>
+            <strong>Album ID</strong>: {track.album_id}
+          </li>
+          <li className='list-group-item'>
+            <strong>Song Genre</strong>:{' '}
+            {track.primary_genres.music_genre_list.length === 0
+              ? 'NO GENRE AVAILABLE'
+              : track.primary_genres.music_genre_list[0].music_genre
+                  .music_genre_name}
+          </li>
+          <li className='list-group-item'>
+            <strong>Explicit Words</strong>:{' '}
+            {track.explicit === 0 ? 'No' : 'Yes'}
+          </li>
+          <li className='list-group-item'>
+            <strong>Updating Date</strong>: {track.updated_time}
+          </li>
+        </ul>
+      </>
+    )
+  }
+}
+
+export default Lyrics
